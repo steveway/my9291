6105905c41f142ab4db2f06d845ca5b78a8eeda7
Stefan Murawski <steveway1@googlemail.com>
2017-10-24 23:34:02 +0200

    Another Test

--- src/my9291.cpp
+++ src/my9291.cpp
@@ -126,7 +126,7 @@ void my9291::_send() {
             duty[0] = _color.white;
             duty[1] = _color.white;
             duty[2] = _color.blue;
-            duty[3] = 0;
+            duty[3] = _color.blue;
             duty[4] = _color.red;
             duty[5] = _color.green;
         } 
